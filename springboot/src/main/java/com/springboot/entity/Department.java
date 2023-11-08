89f8fb51fa4299358e82ad74ee0476b944e599b3
vasudev.parmar <vsdvprmr@gmail.com>
2023-11-08 18:28:55 +0530

    added lombok dependency and removed getter setter methods

--- springboot/src/main/java/com/springboot/entity/Department.java
+++ springboot/src/main/java/com/springboot/entity/Department.java
@@ -6,7 +6,18 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotBlank;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 @Entity
+@Data  
+// @Data annotation will create getter setters tostring methods 
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+// @Builder annotation will implement the  Builder pattern for the Department class
 public class Department {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -14,58 +25,5 @@ public class Department {
     @NotBlank(message = "departmentName cannot be blank")
     String departmentName;
     String departmentAddress;
-
-    public Department() {
-    }
-
     String departmentCode;
-
-    public Department(Long departmentId, String departmentName, String departmentAddress, String departmentCode) {
-        this.departmentId = departmentId;
-        this.departmentName = departmentName;
-        this.departmentAddress = departmentAddress;
-        this.departmentCode = departmentCode;
-    }
-
-    @Override
-    public String toString() {
-        return "Department{" +
-                "DepartmentId=" + departmentId +
-                ", DepartmentName='" + departmentName + '\'' +
-                ", DepartmentAddress='" + departmentAddress + '\'' +
-                ", DepartmentCode='" + departmentCode + '\'' +
-                '}';
-    }
-
-    public Long getDepartmentId() {
-        return departmentId;
-    }
-
-    public void setDepartmentId(Long departmentId) {
-        this.departmentId = departmentId;
-    }
-
-    public String getDepartmentName() {
-        return departmentName;
-    }
-
-    public void setDepartmentName(String departmentName) {
-        this.departmentName = departmentName;
-    }
-
-    public String getDepartmentAddress() {
-        return departmentAddress;
-    }
-
-    public void setDepartmentAddress(String departmentAddress) {
-        this.departmentAddress = departmentAddress;
-    }
-
-    public String getDepartmentCode() {
-        return departmentCode;
-    }
-
-    public void setDepartmentCode(String departmentCode) {
-        this.departmentCode = departmentCode;
-    }
 }
