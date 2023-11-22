1c3bf0ee53a105959be4576e7feb6db1192e289c
vasudev.parmar <vsdvprmr@gmail.com>
2023-11-22 11:34:15 +0530

    getting value from application.config

--- springboot/src/main/java/com/springboot/controller/HelloWorld.java
+++ springboot/src/main/java/com/springboot/controller/HelloWorld.java
@@ -1,5 +1,6 @@
 package com.springboot.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +8,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorld {
 
+    @Value("${welcome.message}")
+    String welcomeMessage;
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String hello() {
-        return "hello";
+        return welcomeMessage;
     }
 }
