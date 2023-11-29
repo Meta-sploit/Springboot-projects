f4a4083e34cfd053c98b1cfd03a33024e8685d4f
vasudev.parmar <vsdvprmr@gmail.com>
2023-11-29 12:37:10 +0530

    added a test case to listthestudent

--- JPATutorial/src/test/java/com/learning/JPATutorial/repository/StudentRepositoryTest.java
+++ JPATutorial/src/test/java/com/learning/JPATutorial/repository/StudentRepositoryTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
 
 @SpringBootTest
 class StudentRepositoryTest {
@@ -14,12 +14,18 @@ class StudentRepositoryTest {
     StudentRepository studentRepository;
     @Test
     public void generateTestForStudentCreation(){
-        Student student=Student.builder().emailId("abhi@gmail.com").firstName("Abhi")
-                .secondName("parmar")
+        Student student=Student.builder().emailId("abhi@gmail.com").firstName("raghu")
+                .secondName("chauhan")
                 .gaurdianEmailId("nikhil@gmail.com")
                 .gaurdianName("Nikhil")
                 .gaurdianMobile("9999999999").build();
         studentRepository.save(student);
+    }
+    
+    @Test
+    public void getAllStudent(){
+        List<Student> studentList= studentRepository.findAll();
+        System.out.println("studentList = " + studentList);
     }
 
 }
